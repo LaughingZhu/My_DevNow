004eb17c2bca939e5c0b2c9059b99aca42836f4f
LaughingZhu <474268433@qq.com>
2024-07-10 14:56:42 +0800

    feat(*): sponsor page

--- src/utils/func.ts
+++ src/utils/func.ts
@@ -3,24 +3,22 @@ type DebounceType = (...args: any[]) => void;
 export function debounce(func: DebounceType, wait: number) {
   let timeout: ReturnType<typeof setTimeout> | null = null;
 
-  // 定义延迟执行的函数，避免在每次调用时都重新创建它
-  const later = function (this: any, args: any[]) {
-    if (timeout) clearTimeout(timeout);
-    try {
-      func.apply(this, args);
-    } catch (error) {
-      console.error('Error executing debounced function:', error);
-      // 可以在这里添加额外的错误处理逻辑，比如错误上报
-    }
-  };
-
   return function executedFunction(this: any, ...args: any[]) {
     const context = this; // 保存调用上下文
 
+    // 定义延迟执行的函数，避免在每次调用时都重新创建它
+    const later = function () {
+      if (timeout) clearTimeout(timeout);
+      try {
+        func.apply(context, args);
+      } catch (error) {
+        console.error('Error executing debounced function:', error);
+        // 可以在这里添加额外的错误处理逻辑，比如错误上报
+      }
+    };
+
     // 如果定时器已经设置，则清除之前的定时器，避免重复调用
     if (timeout) clearTimeout(timeout);
-
-    // 设置新的定时器，延迟执行函数
-    timeout = setTimeout(() => later.apply(context, args), wait);
+    timeout = setTimeout(later, wait);
   };
 }
